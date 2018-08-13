d653c998856870540f5690ab8da439856c8498d8
nakazawaken1 <nakazawaken1@gmail.com>
2018-08-13 14:40:07 +0900

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "PWA Test",
+  "short_name": "PWA",
+  "icons": [{
+    "src": "./icon-256.png",
+    "sizes": "256x256",
+    "type": "image/png"
+  }, {
+    "src": "./icon-192.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }],
+  "start_url": "./?utm_source=homescreen",
+  "display": "standalone"
+}
